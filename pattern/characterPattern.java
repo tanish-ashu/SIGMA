da565f9b198a7db72434f9aecabdc6ef0ad4e2a2
ashu2005rajak@gmail.com <ashu2005rajak@gmail.com>
2025-07-01 03:45:21 +0530

    patterns warmUp with loops

---
+++ pattern/characterPattern.java
@@ -0,0 +1,18 @@
+
+public class characterPattern {
+  public static void main(String[] args) {
+
+    int n = 4;
+    char ch = 'A';
+    // outer loop
+    for (int line = 1; line <= n; line++) {
+      // inner loop
+      for (int chars = 1; chars <= line; chars++) {
+        System.out.print(ch);
+        ch++;
+      }
+      System.out.println();
+    }
+  }
+
+}
