90e89a7c0c51d69303b6d7afaae6fd460a95e877
ashu2005rajak@gmail.com <ashu2005rajak@gmail.com>
2025-07-20 03:22:53 +0530

    incomplete

---
+++ Basic Sorting Algorithms/insertionSort.java
@@ -0,0 +1,22 @@
+import java.util.*;
+// time complexity - O(n2)
+
+public class insertionSort {
+  public static int[] sort(int arr[]){
+    int n = arr.length;
+
+    
+    return arr;
+}
+
+
+  public static void main(String args[]){
+    int array[] = {5,4,1,3,2};
+    int sortedArray[] = sort(array);
+    System.out.println("The sorted Array is:" + Arrays.toString(sortedArray));
+
+
+
+  }
+  
+}
