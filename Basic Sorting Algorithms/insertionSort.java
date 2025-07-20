cc30feef05ad79a03f03582c896bf882b2e3dd0f
ashu2005rajak@gmail.com <ashu2005rajak@gmail.com>
2025-07-21 01:56:12 +0530

    insertionSort revised

--- Basic Sorting Algorithms/insertionSort.java
+++ Basic Sorting Algorithms/insertionSort.java
@@ -3,12 +3,17 @@ import java.util.*;
 
 public class insertionSort {
   public static int[] sort(int arr[]){
-    int n = arr.length;
-
-    
+    for(int i=1; i<arr.length; i++){
+      int key = arr[i];
+      int j = i - 1;
+      while( j >= 0 && arr[j] > key){
+        arr[j+1] = arr[j];
+        j--;
+      }
+      arr[j+1] = key;
+  }
     return arr;
 }
-
 
   public static void main(String args[]){
     int array[] = {5,4,1,3,2};
