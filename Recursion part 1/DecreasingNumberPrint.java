b94e90c121eb086f13d295f69aedbbeabaa830cd
ashu2005rajak@gmail.com <ashu2005rajak@gmail.com>
2025-07-27 02:57:51 +0530

    Recursion introduction

---
+++ Recursion part 1/DecreasingNumberPrint.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class DecreasingNumberPrint{
+  public static void printN(int n){
+    if(n == 1){
+      System.out.println(n);
+      return;
+    }
+    System.out.println(n + " ");
+    printN(n-1);
+  }
+
+
+
+  public static void main(String[] args) {
+    int number = 10;
+    printN(number);
+  }
+}
