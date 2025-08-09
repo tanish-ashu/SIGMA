26a7a9ac47daff0c772ef795a068f2995bbd7a6d
ashu2005rajak@gmail.com <ashu2005rajak@gmail.com>
2025-08-10 04:10:10 +0530

    little String concept / function

---
+++ Assignment Questions/Strings/replaceFunction.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class replaceFunction {
+  public static void main(String[] args) {
+    String str = "Tanishk".replace("T","");
+    System.out.println(str.length());
+    System.out.println(str);
+  }
+  
+}
