25e2a0145f632fed27936636f2a2c53f93e727e3
ashu2005rajak@gmail.com <ashu2005rajak@gmail.com>
2025-09-12 15:12:04 +0530

    Good Question Logic r Backtraiking learnings

---
+++ backtracking/findPermutations.java
@@ -0,0 +1,25 @@
+
+
+public class findPermutations {
+  public static void permutations(String str, String ans){
+    // base case 
+    if(str.length() == 0){
+      System.out.println(ans);
+      return;
+    }
+
+    for( int i = 0; i<str.length(); i++){
+      char curr = str.charAt(i);
+      String newStr = str.substring(0,i) + str.substring(i+1);
+      permutations(newStr, ans + curr);
+    }
+  }
+
+
+  public static void main(String args[]){
+    String str = "abc";
+    permutations(str, "");
+
+  }
+  
+}
